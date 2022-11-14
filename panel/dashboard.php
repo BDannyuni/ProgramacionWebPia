d86cbf2a20e050a3e883a5d21196d5915156e25e
BDannyuni <111476639+BDannyuni@users.noreply.github.com>
2022-11-13 22:56:52 -0600

    Add files via upload

---
+++ panel/dashboard.php
@@ -0,0 +1,40 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Kawschool Store</title>
+
+    <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="../assets/css/estilo1.css">
+	<link rel="stylesheet" href="../assets/css/estilos.css">
+  </head>
+
+  <body>
+  <header>
+        <a href="#" class="logo">LOGO</a>
+
+
+		<input type="checkbox" id="menu-bar">
+		<label for="menu-bar">Menu</label>
+
+        <nav class="navbar">
+            <ul>
+				<li><a href="../index.php">Inicio</a></li>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="../Productos.php">Productos</a></li>
+                <li><a href="Pedidos/index.php">Pedidos</a></li>
+                <li><a href="#">Carrito</a></li>
+            </ul>
+        </nav>
+    </header>
+
+
+  </body>
+</html>
